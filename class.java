9ea7ef5d833b00ff5fa1d2a3089a88a99b254ec3
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-29 05:29:30 -0800

    Add files via upload

---
+++ class.java
@@ -0,0 +1,14 @@
+// Marvish Chandra
+
+
+public static void course(title,time,instructor,booksRequired){
+    if title == "English":
+        System.out.println("Be sure to read the assigned texts and complete your assignments.");
+    if title == "Math":
+        System.out.println("Be sure to review the key concepts of the subject and complete your assignments");
+    if title == "Science":
+        System.out.println("Be sure to review the key concepts of the subject and complete your assignments");
+    System.out.println(time);
+    System.out.println(instructor);
+    System.out.println(booksRequired)
+}
